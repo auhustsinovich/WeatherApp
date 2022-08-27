3685b82a968b32cde8c35cce0b49d7e6201e9048
auhustsinovich <avgustinovich4@gmail.com>
2022-08-27 18:48:03 +0200

    Initial Commit

---
+++ WeatherApp/Model/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Daniil Auhustsinovich on 27.08.22.
+//
+
+import Foundation
---
+++ WeatherApp/ViewControllers/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Daniil Auhustsinovich on 27.08.22.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
